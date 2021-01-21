f271d7e9ca5311339f5eddd2cc050dfbe1b614b8
Imo <imoleadrews@gmail.com>
2021-01-21 21:05:53 +0100

    The branch Imogen needs help with

--- Routes/Login.js
+++ Routes/Login.js
@@ -3,56 +3,105 @@ const client = require("../client");
 const loginRouter = express.Router();
 const jwt = require('jsonwebtoken');
 const authorizeUser = require("../middleware/authorizeUser")
+const bcrypt = require('bcrypt')
+const registerRouter = require('./Register')
 
 
 
-loginRouter.post("/", (req, res) =>{
-    
-    
-        const { email, password } = req.body.loginData;
-    
-        const text = `SELECT * FROM users WHERE email = $1 AND password = $2`;
-        const values = [email, password];
-
-        client.query(text, values)
-        .then(result => {
-            if (!result.rows.length) return  res.status(404).send("this email and password combo don't exist")
-            const { id, email } = result.rows[0]
-           
-            const token = jwt.sign({
-                id,
-                email
-              }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-            //   console.log(result.rows[0])
-            //   console.log(token)
-
-              res.set('x-secret-token', token).send(result.rows)
-        })
-        .catch(error=>{
-            res.status(400).send({bad: error.message})
-        })
+loginRouter.post('/', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  try {
+    const email = req.body.email;
+    const password = req.body.password; 
+    const user = `SELECT * FROM users WHERE email = $4 AND password = $7`
+    if(user) {
+      const validPassword = await bcrypt.compare(password, hashedPassword );
+       if(validPassword) {
+         res.status(200).json('Valid email and password');
+       } else {
+         res.json('Wrong password!');
+    }
+  }
+  } catch(e) {
+    console.log(e);
+    res.status(500).send('Something broke!');
+  }
 })
+// loginRouter.post ("/", async (req, res) => {
+//       const user = `SELECT * FROM users WHERE email = $4 AND password = $7`
+      
+//       if (user == null) {
+//         return res.status(400).send('Cannot find user')
+//       }
 
-loginRouter.post("/me", authorizeUser, (req, res) => {
+//       try{
+//        if( await bcrypt.compare(req.body.password, registerRouter.hashedPassword)) {
+//          res.send('Success') 
+//        } else {
+//          res.send('Not allowed')
+//        }
+//         } catch  {
+//         res.status(500).send()
+//         }
+      
+      
+// })
+
+//  loginRouter.post("/", (req, res) =>{
     
-   const { id, email } = req.user
-
-    const text = `SELECT first_name, surname, email, bio, fav_book FROM users WHERE id = $1 AND email = $2`
-    const values = [id, email]
-
-    client
-        .query(text, values)
-        .then((data) => res.send(data.rows[0]))
-        .catch((err)=>console.log(err))
     
-    /* client
-		.query("SELECT * FROM users")
-		.then((data) => res.json(data.rows))
-		.catch((err) => console.log(err)); */  
-        // res.send(req.user)
+//        const email = req.body.email;
+//        const password =  registerRouter.hashedPassword
+
+//        bcrypt.comapre(req.body.password, password)
+
+
+    
+//      const text = `SELECT * FROM users WHERE email = $4 AND password = $7`;
+//      const values = [email, password];
+
+
+
+//          client.query(text, values)
+//             .then(result => {
+//              if (!result.rows.length) return  res.status(404).send("this email and password combo don't exist")
+//              const { id, email } = result.rows[0]
+           
+//              const token = jwt.sign({
+//                  id,
+//                  email
+//                }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+//              //   console.log(result.rows[0])
+//              //   console.log(token)
+
+//                res.set('x-secret-token', token).send(result.rows)
+//          })
+//          .catch(error=>{
+//              res.status(400).send({bad: error.message})
+//          })
+//  })
+
+//  loginRouter.post("/me", authorizeUser, (req, res) => {
+    
+//     const id = req.body.id;
+//     const email = req.body.email;
+
+//      const text = `SELECT first_name, surname, email, bio, fav_book, password FROM users WHERE id = $1 AND email = $4`
+//      const values = [id, email]
+
+//      client
+//          .query(text, values)
+//          .then((data) => res.send(data.rows[0]))
+//       .catch((err)=>console.log(err))
+    
+//     client
+// 		.query("SELECT * FROM users")
+// 		.then((data) => res.json(data.rows))
+// 		.catch((err) => console.log(err));  
+//          res.send(req.user)
         
-});
+//  });
 
 
 
--- Routes/Register.js
+++ Routes/Register.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const client = require("../client");
 const registerRouter = express.Router();
+const bcrypt = require('bcrypt')
 
+ 
+registerRouter.post("/", async (req, res)=> {
+	
+	
+	const hashedPassword = await bcrypt.hash(req.body.password, 10);
+	
+	console.log(hashedPassword)
 
-
+	//  const { first_name, surname, email, bio, fav_book} = req.body.registerData;
+	
+	
+	
+	const first_name = req.body.first_name ;
+	const surname = req.body.surname;
+	const email =  req.body.email;
+	const bio = req.body.bio;
+	const fav_book =  req.body.fav_book;
+	const password =  hashedPassword;
+	
+	
+	const text = `INSERT INTO users (first_name, surname, email, bio, fav_book, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+	const values = await [ first_name, surname, email, bio, fav_book, password ]
+	
+	
+client.query(text, values) 
+	.then((data)=>res.json(data.rows))
+	.catch((error)=> console.log(error)); 
+	 })
 
 
 /* registerRouter.post("/", (req, res)=> {
@@ -24,19 +51,42 @@ const registerRouter = express.Router();
 	)
 }) */
 
-registerRouter.post("/", (req, res) =>{
-/* console.log(req.body) */
+// registerRouter.post("/", async (req, res) =>{
+// /* console.log(req.body) */
+//     const saltRounds = 10;
+// 	const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+// 	console.log(hashedPassword)
 
-	const { first_name, surname, email, password, bio, fav_book} = req.body.registerData;
+// 	const first_name = req.body.first_name
+// 	const surname = req.body.surname
+// 	const email = req.body.email
+// 	const password = hashedPassword
+// 	const bio = req.body.bio
+// 	const fav_book = req.body.fav_book
 
-	const text = `INSERT INTO users (first_name, surname, email, password, bio, fav_book) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
-	const values = [first_name, surname, email, password, bio, fav_book];
+// 	const text = await `INSERT INTO users (first_name, surname, email, hashedPassword, bio, fav_book) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+// 	const values = [first_name, surname, email, hashedPassword, bio, fav_book];
 
-	client.query(text, values)
-	.then((data)=>res.json(data.rows))
-	.catch((error)=> console.log(error)); 
+// 	client.query(text, values)
+// 	.then((data)=>res.json(data.rows))
+// 	.catch((error)=> console.log(error)); 
 
-});
+// });
+
+// registerRouter.post("/", (req, res) =>{
+// 	/* console.log(req.body) */
+	
+// 		const { first_name, surname, email, password, bio, fav_book} = req.body.registerData;
+	
+// 		const text = `INSERT INTO users (first_name, surname, email, password, bio, fav_book) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+// 		const values = [first_name, surname, email, password, bio, fav_book];
+	
+// 		client.query(text, values)
+// 		.then((data)=>res.json(data.rows))
+// 		.catch((error)=> console.log(error)); 
+	
+// 	});
+	
 
 
 
